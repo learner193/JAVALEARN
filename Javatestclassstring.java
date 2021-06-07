d99b72075ada6eb5e7076810c902457f6a093412
DemocracyRevolution <33489083+learner193@users.noreply.github.com>
2021-06-07 22:23:46 +0800

    Add files via upload

---
+++ Javatestclassstring.java
@@ -0,0 +1,19 @@
+package packagetest7;
+
+public class Javatestclassstring {
+	public static void main(String[] argv){
+		String aba=new String();
+		String abb=new String("ddasdda");
+		aba="ssdadadad";
+		System.out.println(aba);
+		System.out.println(abb);
+		System.out.println(abb.length());
+		System.out.println(abb.lastIndexOf('d'));
+		System.out.println(abb.startsWith("ddas"));
+		System.out.println(abb.endsWith("a"));
+		System.out.println(abb.toUpperCase());
+		System.out.println(abb.toUpperCase());
+		System.out.println("\u0048");
+		System.out.println(abb.charAt(2));
+	}
+}
